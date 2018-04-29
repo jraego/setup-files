62ed0a83afbded69622e3abc2ac345a11d661c16
jraego <jraego@gmail.com>
2018-04-29 13:21:12 -0400

    x

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -7,13 +7,14 @@ sudo apt install git -y
 sudo apt install apache2 -y
 sudo apt install php7.0 libapache2-mod-php7.0 -y
 sudo apt install mysql-server -y
+sudo snap install keepassxc
 
 # install nvm and node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 sudo nvm install 8.11.1
 
 # install atom
-#sudo add-apt-repository ppa:webupd8team/atom -y
+#sudo add-apt-repository ppa:webupd8team/atom
 #sudo update
 #sudo apt install atom
 
@@ -31,9 +32,8 @@ git clone https://github.com/jraego/setup-files
 cd ~
 ln my-setup-files/.vimrc .vimrc
 
-sudo /etc/init.d/apache2 restart 
+sudo /etc/init.d/apache2 restart
 # test that apache is working with php
 #sudo rm /var/www/html/index.html
 #sudo touch /var/www/html/index.php
 #sudo echo '<?php echo 'Hello!' ?>' > /var/www/html/index.php
-
